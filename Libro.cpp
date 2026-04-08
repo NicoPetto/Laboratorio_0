f9c7e27d99703bc2c30761c73a61235c2b60f048
Elizeth <elizethcedres@gmail.com>
2026-04-08 17:54:41 -0300

    Update Libro.cpp

--- Libro.cpp
+++ Libro.cpp
@@ -3,3 +3,13 @@
 //
 
 #include "Libro.h"
+
+Libro::~Libro(){
+cout<<"Libro:"<<this->titulo<<" destruido"<<endl;
+}
+Libro::Libro(string DOI, string titulo, DTFecha fecha, string editorial, string palabrasDestacadas)
+    : Publicacion(DOI, titulo, fecha), editorial(editorial), palabrasDestacadas(palabrasDestacadas)
+{}
+bool Libro::contienePalabra(string palabra){
+
+}
