68d66a73c125502d7fe7dedbebfd3cc008018b3a
Elizeth <elizethcedres@gmail.com>
2026-04-08 17:51:57 -0300

    Update ArticuloRevista.cpp

--- ArticuloRevista.cpp
+++ ArticuloRevista.cpp
@@ -3,3 +3,14 @@
 //
 
 #include "ArticuloRevista.h"
+ArticuloRevista::~ArticuloRevista() {
+    cout<<"Articulo Revista: "<<this->revista<<" destruido"<<endl;
+}
+
+ArticuloRevista::ArticuloRevista(string DOI, string titulo, DTFecha fecha, string revista, string extracto)
+: Publicacion(DOI, titulo, fecha), revista(revista), extracto(extracto)
+{}
+
+bool ArticuloRevista::contienePalabra(string palabra) {
+
+}
