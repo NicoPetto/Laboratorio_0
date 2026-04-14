ad9cb3280fa46ba6f7693aa8c080a6f869545cef
Elizeth <elizethcedres@gmail.com>
2026-04-14 11:19:18 -0300

    Update PaginaWeb.cpp

--- PaginaWeb.cpp
+++ PaginaWeb.cpp
@@ -4,7 +4,9 @@
 
 #include "PaginaWeb.h"
 
-
+PaginaWeb::~PaginaWeb(){
+    cout<<"PaginaWeb destruida"<<endl;
+}
 PaginaWeb::PaginaWeb(string DOI, string titulo, DTFecha fecha, string url, string contenidoExtraido)
     : Publicacion(DOI, titulo, fecha),
       url(url),
