b47407f3ef1f8b63cccf2ff00f3bb28b6fd2ffd4
Elizeth <elizethcedres@gmail.com>
2026-04-14 11:11:21 -0300

    Update Libro.cpp

--- Libro.cpp
+++ Libro.cpp
@@ -1,15 +1,14 @@
 //
-// Created by Nicolás on 27/3/2026.
+// Created by elize on 3/30/2026.
 //
-
 #include "Libro.h"
 
 Libro::~Libro(){
 cout<<"Libro:"<<this->titulo<<" destruido"<<endl;
 }
-Libro::Libro(string DOI, string titulo, DTFecha fecha, string editorial, string palabrasDestacadas)
+Libro::Libro(string DOI, string titulo, DTFecha fecha, string editorial, set<string> palabrasDestacadas)
     : Publicacion(DOI, titulo, fecha), editorial(editorial), palabrasDestacadas(palabrasDestacadas)
 {}
 bool Libro::contienePalabra(string palabra){
-
+   return palabrasDestacadas.find(palabra)!=palabrasDestacadas.end();
 }
